f8e129e806fdd03f2c95f974cbe83b8a6d58fdf9
unknown <guilhermealex01@gmail.com>
2020-12-03 23:20:37 -0300

    atualizando crud back end

--- STS/Crud Back end/Blog_Pessoal/src/main/java/com/example/demo/PostagemController.java
+++ STS/Crud Back end/Blog_Pessoal/src/main/java/com/example/demo/PostagemController.java
@@ -1,7 +1,5 @@
 package com.example.demo;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,30 +12,31 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController //Informando a Spring que é o controlador
-@RequestMapping("/Postagem") //
-@CrossOrigin("*") //Aceitando requicisoes de qualquer origem
-public class PostagemController 
+@RestController //informa para o spring que ele é um controlador
+@RequestMapping("/postagens")// qual é a RI(oq esta dentro ("/") que essa classe será acessada como a principal
+@CrossOrigin("*")//Ele vai aceitar requicoes de qlq origin
+public class PostagemController
 {
-	@Autowired //Instanciando a interface, injeção
+	@Autowired //para instanciar essa interface, injecao
 	private PostagemRepository repository;
-
+	
 	@GetMapping
-	public ResponseEntity<List<PostagemModel>> GetAll() {
+	public ResponseEntity<List<PostagemModel>> GetAll()
+	{
 		return ResponseEntity.ok(repository.findAll());
-		
 	}
-	@GetMapping("/{id}")
-	public ResponseEntity<PostagemModel> GetById(@PathVariable Long id){
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	@GetMapping ("/{id}")
+	public ResponseEntity<PostagemModel>GetById(@PathVariable long id)
+	{
+			return repository.findById(id)
+					.map(resp ->ResponseEntity.ok(resp))
+					.orElse(ResponseEntity.notFound().build());
+		
 	}
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<PostagemModel>>GetByTitulo(@PathVariable String titulo)
 	{
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
-				
 	}
 	@PostMapping
 	public ResponseEntity<PostagemModel> post (@RequestBody PostagemModel postagem)
@@ -49,9 +48,10 @@ public class PostagemController
 	{
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
-	@DeleteMapping("/{id}")
-	public void delete (@PathVariable Long id ) 
+	@DeleteMapping("/{id}")// void nao vai retornar nada
+	public void delete(@PathVariable long id)
 	{
-		repository.deleteById(id);
+		repository.deleteById(id);//como ele é um void nao tem return
 	}
+	
 }
--- STS/Crud Back end/Blog_Pessoal/src/main/java/com/example/demo/PostagemRepository.java
+++ STS/Crud Back end/Blog_Pessoal/src/main/java/com/example/demo/PostagemRepository.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
-public interface PostagemRepository extends JpaRepository<PostagemModel, Long >
+public interface PostagemRepository extends JpaRepository<PostagemModel, Long>
 {
-public List<PostagemModel> findAllByTituloContainingIgnoreCase (String titulo);
-
-
-
+	public List<PostagemModel> findAllByTituloContainingIgnoreCase (String titulo);
 }
+
